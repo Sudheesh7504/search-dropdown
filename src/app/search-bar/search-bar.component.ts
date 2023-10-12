ac779493cbb4c90fa6bb333738625c8fc8167534
Sudheesh7504 <ksudeesh143@gmail.com>
2023-10-12 12:40:07 +0530

    suggestions

--- src/app/search-bar/search-bar.component.ts
+++ src/app/search-bar/search-bar.component.ts
@@ -1,52 +1,3 @@
-// import { Component } from '@angular/core';
-// import { SearchService } from '../search.service';
-
-// @Component({
-//   selector: 'app-search-bar',
-//   templateUrl: './search-bar.component.html',
-//   styleUrls: ['./search-bar.component.scss'],
-// })
-// export class SearchbarComponent {
-//   searchQuery: string = '';
-//   isAscending: boolean = true;
-//   showSuggestions: boolean = false;
-//   suggestions: string[] = [];
-
-//   toggleSuggestions() {
-//     this.showSuggestions = !this.showSuggestions;
-//     this.updateSuggestions();
-//   }
-
-//   searchResults$ = this.searchService.searchResults$;
-//   constructor(private searchService: SearchService) { }
-
-//   updateSuggestions() {
-//     if (this.searchQuery.trim() !== '') {
-//       this.suggestions = [
-//         'Task 1',
-//         'Task 2',
-//         'Task 3',
-//         'Task 4',
-//         'Task 5',
-//       ].filter((task) =>
-//         task.toLowerCase().includes(this.searchQuery.toLowerCase())
-//       );
-//     } else {
-//       this.suggestions = [];
-//     }
-//     this.searchService.fetchSearchResults(this.searchQuery);
-//   }
-
-//   toggleDirection() {
-//     this.isAscending = !this.isAscending;
-//     this.toggleSuggestions();
-//   }
-
-//   selectSuggestion(suggestion: string) {
-//     this.searchQuery = suggestion;
-//     this.showSuggestions = false;
-//   }
-// }
 
 
 import { Component } from '@angular/core';
@@ -63,6 +14,7 @@ export class SearchbarComponent {
   suggestions: string[] = [];
   searchResults: { [key: string]: string[] } = {};
   isAscending: boolean = true;
+  lastSelectedSuggestion: string = ''; // Track the last selected suggestion
 
   constructor(private searchService: SearchService) { }
 
@@ -81,10 +33,8 @@ export class SearchbarComponent {
       this.suggestions = [];
     }
 
-    // Fetch individual search results for each suggestion
     this.suggestions.forEach((suggestion) => {
       this.searchService.fetchSearchResults(suggestion).subscribe((results) => {
-        // Store results in a map
         this.searchResults[suggestion] = results;
       });
     });
@@ -93,15 +43,26 @@ export class SearchbarComponent {
   selectSuggestion(suggestion: string) {
     this.searchQuery = suggestion;
     this.showSuggestions = false;
+
+    // Store the last selected suggestion
+    this.lastSelectedSuggestion = suggestion;
   }
 
   toggleSuggestions() {
     this.showSuggestions = !this.showSuggestions;
     this.updateSuggestions();
   }
+
   toggleDirection() {
     this.isAscending = !this.isAscending;
     this.toggleSuggestions();
   }
+
+  // Function to open the search bar and display the last selected suggestion
+  openSearchBar() {
+    this.searchQuery = this.lastSelectedSuggestion;
+    this.showSuggestions = true;
+  }
 }
+
 
--- src/app/search.service.ts
+++ src/app/search.service.ts
@@ -36,22 +36,19 @@ export class SearchService {
   searchResults$ = this.searchResultsSubject.asObservable();
 
   fetchSearchResults(query: string): Observable<string[]> {
-    // In a real application, you would make an HTTP request to fetch search results for the given query.
-    // For this example, we'll simulate it with a delay.
+
     const searchResults = this.generateSearchResults(query);
 
-    // Simulate a delay (replace with actual HTTP request)
     return new Observable<string[]>((observer) => {
       setTimeout(() => {
         observer.next(searchResults);
         observer.complete();
-      }, 1000); // Simulate a 1-second delay
+      }, 1000);
     });
   }
 
   private generateSearchResults(query: string): string[] {
-    // Generate some mock search results for the given query.
-    // You can replace this with your actual search logic.
+
     return [
       `Result 1 for ${query}`,
       `Result 2 for ${query}`,
